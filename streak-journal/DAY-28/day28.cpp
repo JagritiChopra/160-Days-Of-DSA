31c3207fdbf76a6e6ea72749d5f3c6a905fee084
JAgriti Chopra <jagritichopra18@gmail.com>
2025-05-09 21:23:58 +0530

    Brute Force as well as optimised solution added

--- streak-journal/DAY-28/day28.cpp
+++ streak-journal/DAY-28/day28.cpp
@@ -8,12 +8,21 @@ Examples :
 Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 2
 Output: 4
 Explanation: target = 2 occurs 4 times in the given array so the output is 4.
+
 Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 4
 Output: 0
 Explanation: target = 4 is not present in the given array so the output is 0.
+
 Input: arr[] = [8, 9, 10, 12, 12, 12], target = 12
 Output: 3
 Explanation: target = 12 occurs 3 times in the given array so the output is 3.
+
+Constraints:
+1 ≤ arr.size() ≤ 106
+1 ≤ arr[i] ≤ 106
+1 ≤ target ≤ 106
+
+
 */
 
 /* solution */
@@ -46,4 +55,70 @@ int main() {
     cout<< countFreq(arr, target);
     return 0;
 }
+
+/* better approach 
+Binary search 
+
+| Aspect | Complexity     |
+| ------ | -------------- |
+| Time   | `O(log n + k)` |
+| Space  | `O(1)`         |  */
+
+
+int countFreq(vector<int> &arr, int target) {
+    int low = 0 , high = arr.size()-1;
+    int count=0;
+
+    while(low <= high ){
+        int mid = low + (high-low)/2 ;
+        
+        if(arr[mid]==target){
+            int leftcount=mid ,  rightcount=mid;
+
+            //left-count
+            while(leftcount>=0 && arr[leftcount]==target){
+                count++;
+                leftcount--;
+            }
+        
+
+            //right-count
+            rightcount=mid+1;
+            while(rightcount<arr.size() && arr[rightcount]==target){
+                count++;
+                rightcount++;
+            } return count;
+        }
+
+        else if(arr[mid] < target){
+            low = mid+1;
+        }
+
+        else{
+            high= mid-1;
+        }
+    }
+
+    return 0;
+}
+
+/* Optimised Approach*/
+
+// O(logn) Time and O(1) Space
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int countFreq(vector<int>& arr, int target) {
+    int l = lower_bound(arr.begin(), arr.end(), target) - arr.begin();  //lower_bound: returns iterator to the first element ≥ target
+    int r = upper_bound(arr.begin(), arr.end(), target) - arr.begin();  //upper_bound: returns iterator to the first element > target
+    
+    // The difference gives the number of occurrences
+    return r - l;
+}
+
+
+
+
 
