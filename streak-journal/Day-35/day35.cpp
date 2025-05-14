a211963e091f56571645c544a621091d976b84d8
JAgriti Chopra <jagritichopra18@gmail.com>
2025-05-14 20:36:01 +0530

    problem statement added

---
+++ streak-journal/Day-35/day35.cpp
@@ -0,0 +1,18 @@
+/* Problem Statement :
+
+Kth Missing Positive Number in a Sorted Array
+Given a sorted array of distinct positive integers arr[] and integer k, the task is to find the kth positive number that is missing from arr[].
+
+Examples : 
+
+Input: arr[] = [2, 3, 4, 7, 11], k = 5
+Output: 9
+Explanation: Missing are 1, 5, 6, 8, 9, 10, ... and 5th missing number is 9.
+
+Input: arr[] = [1, 2, 3], k = 2
+Output: 5
+Explanation: Missing are 4, 5, 6.... and 2nd missing number is 5.
+
+Input: arr[] = [3, 5, 9, 10, 11, 12], k = 2
+Output: 2
+Explanation: Missing are 1, 2, 4, 6, 7, 8, 13,... and 2nd missing number is 2.*/
