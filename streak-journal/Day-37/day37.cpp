45aa2575878c8ed9aba994005fade075ca5add34
JAgriti Chopra <jagritichopra18@gmail.com>
2025-05-17 21:02:37 +0530

    poblem statement added

---
+++ streak-journal/Day-37/day37.cpp
@@ -0,0 +1,22 @@
+/* Problem Statement :
+
+Rotate Square Matrix by 90 Degrees Counterclockwise
+Given a n*n square matrix mat[][], rotate it by 90 degrees in counterclockwise direction without using any extra space.
+
+Examples: 
+
+Input: mat[][] = [[1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]]
+Output: [[3, 6, 9], 
+                [2, 5, 8], 
+                [1, 4, 7]]
+
+Input: mat[][] = [[1, 2,  3, 4],
+            [5, 6, 7,  8], 
+            [9, 10, 11, 12],
+            [13, 14, 15, 16]]
+Output: [[4, 8, 12, 16], 
+            [3, 7, 11, 15],
+            [2, 6, 10, 14],
+            [1, 5, 9, 13]]*/
