795715d9921372411571930ff65fcc6fe17c9025
JAgriti Chopra <jagritichopra18@gmail.com>
2025-05-27 15:45:59 +0530

    problem statement added

--- streak-journal/DAY-05/day5.cpp
+++ streak-journal/DAY-05/day5.cpp
@@ -1,7 +1,7 @@
 /* Problem statement :
 
 Next Permutation
-Difficulty: MediumAccuracy: 40.66%Submissions: 209K+Points: 4Average Time: 20m
+Difficulty: Medium
 Given an array of integers arr[] representing a permutation, implement the next permutation that rearranges the numbers into the lexicographically next greater permutation. If no such permutation exists, rearrange the numbers into the lowest possible order (i.e., sorted in ascending order). 
 
 Note - A permutation of an array of integers refers to a specific arrangement of its elements in a sequence or linear order.
